58eaa5db69260e38decc44d1f636729c00dc3f4d
Seo Hyun Baek <hyunny0463@gmail.com>
2019-03-21 14:15:18 +0900

    simulation complexity O(N + L) -> O(N)

--- BOJ/2980_도로와_신호등.cpp
+++ BOJ/2980_도로와_신호등.cpp
@@ -1,10 +1,15 @@
 /*
  * 2980. 도로와 신호등
  *
- * 시간복잡도 : O(N * L)
+ * 시간복잡도 : O(N)
  *
- * 신호등 정보를 모두 저장한 뒤 매 시간마다 모든 위치의 신호등의 정보를 갱신하면서
- * 상근이가 지나갈 수 있는지 판단하고, 상근이가 L 위치에 도달하면 종료한다.
+ * 처음에는 모든 신호등의 위치를 입력받고 한칸씩 이동하며 신호등의 모든 상태를 갱신하며
+ * 최종 L 까지 도달하며 걸린 시간을 측정했지만
+
+ * 문제에서 신호등은 D가 증가하는 순서로 주어진다는 조건을 통해
+ * 입력을 받음과 동시에 해당 신호등까지 걸리는 시간과
+ * 그 위치에서 빨간색 신호등이라면 기다려야 하는시간을 더해주고
+ * 마지막으로 현재위치에서 L까지 시간을 더해주는 방식으로 문제를 해결함
  *
  */
 
@@ -14,39 +19,25 @@
 #include <vector>
 using namespace std;
 
-struct light { // 신호등, 빨간불이 켜지는 시간, 파란불이 켜지는 시간
-	int D, R, G;
-};
-
 int main(int argc, char** argv)
 {
 	int N, L;
 	cin >> N >> L; // 신호등 개수, 도로의 길이
 
-	vector<int> road(L + 1, 1); // 0: 빨간불, 1: 도로, 2: 파란불
-	vector<light> tl; // traffic light
+	int t = 0; // 걸린 시간
+	int pos = 0; // 상근이 위치
+	for (int i = 0, D, R, G, rest; i < N; ++i) {
+		cin >> D >> R >> G; // 신호등 위치, 빨간불 켜지는 시간, 초록불 켜지는 시간
 
-	for (int i = 0, D, R, G; i < N; ++i) {
-		cin >> D >> R >> G;
-		tl.push_back({ D, R, G }); // 벡터에 모든 신호등을 넣음
-		road[D] = 0;
+		t += (D - pos); // 현재 위치에서 입력받은 신호등까지 걸리는 시간
+		pos = D; // 현재 위치는 신호등으로 갱신
+
+		rest = t % (R + G); // 그 시간에 신호등의 상태를 알아보기 위한 변수
+		t += rest < R ? R - rest : 0; // R보다 작다면 빨간불이므로 R - rest는 빨간불에서 기다려야 하는 시간
 	}
+	t += (L - pos); // 마지막 위치에서 L 까지 걸리는 시간
 
-	int cnt = 0, sang = 1;
-	while (++cnt) { // 상근이가 L 까지 갈 때까지 걸리는 시간
-		if (sang == L)
-			break;
-		if (road[sang]) // 빨간불이 아니면
-			sang++;
-		for (int i = 0; i < tl.size(); ++i) { // 매 시간 모든 신호등의 상태를 갱신함
-			if (!road[tl[i].D] && (cnt % (tl[i].R + tl[i].G)) == tl[i].R)
-				road[tl[i].D] = 2;
-			else if (road[tl[i].D] == 2 && (cnt % (tl[i].R + tl[i].G)) == 0)
-				road[tl[i].D] = 0;
-		}
-	}
-
-	cout << cnt - 1 << '\n'; // 처음 1을 더한 시간이므로
+	cout << t << '\n';
 
 	return 0;
 }
